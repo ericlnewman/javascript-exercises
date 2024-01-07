03df66639233335fb902ac30db041d547570aa2d
ericlnewman <eric.leon.newman@gmail.com>
2024-01-06 20:24:26 -0500

    Updated function and removed console logs

--- 04/removeFromArray.js
+++ 04/removeFromArray.js
@@ -1,22 +1,11 @@
 const removeFromArray = function(nums, ...n) {
-    function remove(arr1, arr2){
-        const newArr = [];
-        let  k = 0;
-        for(let i = 0; i < arr1.length; i++){
-            for(let j = k; j < arr2.length; j++){
-                if(arr1[i] !== arr2[j] && !newArr.includes(arr1[i])){
-                    newArr[k] = arr1[i];
-                    k++;
-                }
+    for(let i = 0; i < nums.length; i++){
+        for(let number of n){
+            if(nums[i] === number){
+                nums.splice(i, 1);
             }
         }
-        return newArr;
     }
-    return remove(nums, n);
+    return nums;
 };
-
-console.log(removeFromArray([1,2,3,4,5], 3));
-console.log(removeFromArray([1, 2, 3, 4], 3, 2));
-console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
-console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 module.exports = removeFromArray;
