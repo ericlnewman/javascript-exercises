08c0669b40106b3edddbf612bdfc5c55cf61b01d
ericlnewman <eric.leon.newman@gmail.com>
2024-01-24 22:55:03 -0500

    solved exercise using the built in map function to return an array of the books' titles

---
+++ 11/getTheTitles.js
@@ -0,0 +1,6 @@
+const getTheTitles = function(books) {
+    const titleOfBook = books.map((book)=> book.title);
+    return titleOfBook;
+};
+// Do not edit below this line
+module.exports = getTheTitles;
